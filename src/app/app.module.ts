5b3d7312f3d92286f36f9d60514278c109e103d6
Jamal Guyo <jamalguyo08@gmail.com>
2018-10-22 13:41:38 +0300

    add material component and setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
+//modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material';
+//components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BindingComponent } from './binding/binding.component';
-import { AppRoutingModule } from './app-routing.module';
 import { DirectiveComponent } from './directive/directive.component';
 import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
 import { FormComponent } from './form/form.component';
+//directives
 import { MagentablackDirective } from './directive/magentablack.directive';
+import { MaterialComponent } from './material/material.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,9 +24,17 @@ import { MagentablackDirective } from './directive/magentablack.directive';
     DirectiveComponent,
     PageNotFoundComponentComponent,
     FormComponent,
-    MagentablackDirective
+    MagentablackDirective,
+    MaterialComponent
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
